49d1ac60aab7030585bc194b36772d007272bd1a
AAAAAA <wr_1895@tju.edu.cn>
2026-02-02 06:05:17 +0000

    Update ncars_framing.py

--- preprocess/ncars_framing.py
+++ preprocess/ncars_framing.py
@@ -8,8 +8,10 @@ from src.io.psee_loader import PSEELoader
 
 def parse_args():
     parser = argparse.ArgumentParser(description="N-CARS framing")
-    parser.add_argument("-rp", "--root_path", type=str, default="/dvs_dataset/N-CARS/original")
-    parser.add_argument("-sp", "--save_path", type=str, default="/dvs_dataset/N-CARS")
+    #parser.add_argument("-rp", "--root_path", type=str, default="/dvs_dataset/N-CARS/original")
+    #parser.add_argument("-sp", "--save_path", type=str, default="/dvs_dataset/N-CARS")
+    parser.add_argument("-rp", "--root_path", type=str, default="/kaggle/input/ncars-dataset/original")
+    parser.add_argument("-sp", "--save_path", type=str, default="/kaggle/input/ncars-dataset/")
     parser.add_argument("-dc", "--data_class", type=str, default="cars", help="cars or background")
     parser.add_argument("-dm", "--data_mode", type=str, default="train", help="train or test")
     return parser.parse_args()
