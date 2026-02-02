3d266f877333e52f8d684cfc1ea897eca7c0623e
AAAAAA <wr_1895@tju.edu.cn>
2026-02-02 06:10:55 +0000

    Update test_ncars.py

--- test_ncars.py
+++ test_ncars.py
@@ -13,7 +13,8 @@ from utils.datasets import Resize_frame, NCARS_SBT
 def parse_args():
     parser = argparse.ArgumentParser(description="N-CARS Classification")
     
-    parser.add_argument("--data_path", type=str, default="/dvs_dataset/N-CARS")
+#    parser.add_argument("--data_path", type=str, default="/dvs_dataset/N-CARS")
+    parser.add_argument("--data_path", type=str, default="/kaggle/input/ncars-dataset")
     parser.add_argument("--device", default="0", help="CUDA device, i.e. 0 or cpu")
     
     # Basic setting
